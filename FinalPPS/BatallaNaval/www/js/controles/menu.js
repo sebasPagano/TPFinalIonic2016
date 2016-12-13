194fe92a7a2e88e95e02c97fa33ea9e2be39513b
sebasPagano <sebas_el14@live.com.ar>
2016-12-13 15:44:50 -0300

    Login con mail y password, pagina autor, Para batalla naval

---
+++ FinalPPS/BatallaNaval/www/js/controles/menu.js
@@ -0,0 +1,22 @@
+
+angular.module('menu.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,$state) {
+
+	/*$scope.UsuarioLogueado = {};
+  	$scope.UsuarioLogueado=firebase.auth();
+  	console.info("usuario",$scope.UsuarioLogueado);
+	//console.info($scope.UsuarioLogueado.email);
+
+  $scope.LogOut = function(){
+
+    firebase.auth().signOut().catch(function(Error){
+      console.log("Error: ", Error);
+    }).then(function(Respuesta){
+      console.log("Respuesta: ", Respuesta);
+	$state.go("login");
+
+    });
+	}*/
+
+});
