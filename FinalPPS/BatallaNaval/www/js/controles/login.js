c70639584e05875491b1a741e259f7a18fddf1d1
sebasPagano <sebas_el14@live.com.ar>
2016-12-13 20:23:25 -0300

    Registro Usuario Batalla naval

--- FinalPPS/BatallaNaval/www/js/controles/login.js
+++ FinalPPS/BatallaNaval/www/js/controles/login.js
@@ -38,6 +38,12 @@ angular.module('login.controllers', ['ngCordova'])
           console.log("Error: ", Error);
       });
   }
+        $scope.Registro = function()
+  {
+  var user = firebase.auth().createUserWithEmailAndPassword($scope.loginData.username,$scope.loginData.password);
+ // alert("Registrado Exitosamente");
+  $state.go('app.autor');
+  }
 
    $scope.Acceso = function(correo, clave){
         $scope.loginData.username  = correo;
